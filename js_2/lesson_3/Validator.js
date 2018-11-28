1da2fa82bf0a2cdb407c7275a259a8684f1dff11
EmptyWave <dimonyud@gmail.com>
2018-11-28 17:22:22 +0300

    js-2 lesson-3 homework completed

--- js_2/lesson_3/Validator.js
+++ js_2/lesson_3/Validator.js
@@ -1,4 +1,4 @@
-class Validator {
+class Validator{
   constructor(id, source, regExp, replaceSymbol){
     this.id = id;
     this._getText(source, regExp, replaceSymbol);
@@ -13,5 +13,36 @@ class Validator {
   _render(id, text){
     let el = document.getElementById(id);
     el.innerText = text;
+  }
+}
+
+class FormValidator{
+  constructor(settings){
+    this.settings = settings;
+  }
+  check(inputs){
+    let errorFields = [];
+    for (let key in inputs){
+      const value = inputs[key].value;
+      if (this._valueIsValid(value,key)) {
+        if (inputs[key].classList.contains('input__err')) inputs[key].classList.remove('input__err');
+        inputs[key].classList.add('input__ok');
+      } else {
+        event.preventDefault();
+        errorFields.push(inputs[key].placeholder);
+        if (inputs[key].classList.contains('input__ok')) inputs[key].classList.remove('input__ok');
+        inputs[key].classList.add('input__err');
+      }
+    }
+    if (errorFields.length > 0){
+      let str = "";
+      for (let name of errorFields){
+        str += `${name}, `;
+      }
+      alert(`Не верно введены данные в полях: ${str}пожалуйста исправьте и попробуйте снова.`)
+    }
+  }
+  _valueIsValid(value,key){
+    return value.match(this.settings.regExp[key]);
   }
 }
